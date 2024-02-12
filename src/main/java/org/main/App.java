01f777c6f58c9b204506fca008d47e09e8b479a3
Pavlo Barabah <99317379+Barabah26@users.noreply.github.com>
2024-02-12 18:41:41 +0200

    created packages (#2)

--- src/main/java/org/main/App.java
+++ src/main/java/org/main/App.java
@@ -4,11 +4,9 @@ package org.main;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!!!!" );
-        System.out.println( "Hello Friends!!!!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!!");
+        System.out.println("Hello Friends!!!!");
     }
 }
