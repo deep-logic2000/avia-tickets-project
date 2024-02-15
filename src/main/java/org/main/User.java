8b74fa9b64b8a3ca48b4861df5fe0b5f1d1c2c3f
pavlobarabah <pavlobarabah@gmail.com>
2024-02-15 19:35:51 +0200

    redone the authentication

--- src/main/java/org/main/User.java
+++ src/main/java/org/main/User.java
@@ -1,6 +1,7 @@
 package org.main;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class User implements Serializable {
 
@@ -29,6 +30,20 @@ public class User implements Serializable {
                 ", name='" + name + '\'' +
                 ", surname='" + surname + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(login, user.login) && Objects.equals(password, user.password);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login, password);
     }
 
     public String getLogin() {
