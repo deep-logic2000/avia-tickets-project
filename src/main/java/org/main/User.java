e37c13b8b236b25fce46c62ebe4e9211f23a978c
Kirill N <95580392+deep-logic2000@users.noreply.github.com>
2024-02-16 21:30:16 +0200

    Create flight dao (#5)
    * Change log
    
    * Added Flight DAO, service, controller
    
    * Added DAO layer
    
    * Added methods to controller & service
    
    * Fix: handle exeption error
    
    ---------
    
    Co-authored-by: Kirill Nazarov <urij7762@gmail.com>

--- src/main/java/org/main/User.java
+++ src/main/java/org/main/User.java
@@ -1,4 +1,13 @@
 package org.main;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
+    String name;
+    String surName;
+
+    public User(String name, String surName) {
+        this.name = name;
+        this.surName = surName;
+    }
 }
